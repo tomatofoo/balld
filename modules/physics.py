3107137cf002fb10ebb05d80baad7bd62502e65b
Tomatofu <99457089+tomatofoo@users.noreply.github.com>
2026-06-15 01:11:18 -0700

    add objects stuff

--- modules/physics.py
+++ modules/physics.py
@@ -20,5 +20,19 @@ class Object(object):
 
 class Level(object):
     def __init__(self: Self, objects: set[Object]) -> None:
-        pass
+        self._objects = objects
+        for obj in objects:
+            obj._level = self
+    
+    @property
+    def objects(self: Self) -> set[Object]:
+        return self._objects
+
+    @objects.setter
+    def objects(self: Self, value: set[Object]) -> None:
+        for obj in self._objects:
+            obj._level = None
+        self._objects = objects
+        for obj in objects:
+            obj._level = self
 
