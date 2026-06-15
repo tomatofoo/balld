0a11df4596cc4dc4dd959e44965138233dd35dd5
Tomatofu <99457089+tomatofoo@users.noreply.github.com>
2026-06-15 00:51:03 -0700

    add main.py

---
+++ main.py
@@ -0,0 +1,55 @@
+import time
+from typing import Self
+
+import pygame as pg
+
+
+class Game(object):
+
+    _SCREEN_SIZE = (640, 480)
+    _SURF_RATIO = (2, 2)
+    _SURF_SIZE = (int(_SCREEN_SIZE[0] / _SURF_RATIO[0]),
+                  int(_SCREEN_SIZE[1] / _SURF_RATIO[1]))
+    _SCREEN_FLAGS = pg.RESIZABLE | pg.SCALED
+    _GAME_SPEED = 60
+
+    def __init__(self: Self) -> None:
+        pg.init()
+
+        self._settings = {
+            'vsync': 1,
+        }
+        self._screen = pg.display.set_mode(
+            self._SCREEN_SIZE,
+            flags=self._SCREEN_FLAGS,
+            vsync=self._settings['vsync']
+        )
+        pg.display.set_caption('Balld')
+        self._surface = pg.Surface(self._SURF_SIZE)
+        self._running = 0
+
+    def run(self: Self) -> None:
+        self._running = 1
+        start_time = time.time()
+
+        while self._running:
+            delta_time = time.time() - start_time
+            start_time = time.time()
+
+            rel_game_speed = delta_time * self._GAME_SPEED
+
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    self._running = 0
+
+            resized_surf = pg.transform.scale(self._surface, self._SCREEN_SIZE)
+            self._screen.blit(resized_surf, (0, 0))
+
+            pg.display.update()
+
+        pg.quit()
+
+
+if __name__ == '__main__':
+    Game().run()
+
