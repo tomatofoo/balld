c5811bd5c6bf0eef793939713e7110867c048877
Tomatofu <99457089+tomatofoo@users.noreply.github.com>
2026-06-15 01:04:22 -0700

    what the fuck

--- modules/physics.py
+++ modules/physics.py
@@ -5,8 +5,17 @@ import pygame as pg
 
 
 class Object(object):
-    def __init__(self: Self) -> None:
+    def __init__(self: Self, pos: pg.Vector2) -> None:
         self._level = None
+        self._pos = pos
+
+    @property
+    def pos(self: Self) -> pg.Vector2:
+        return self._pos
+    
+    @pos.setter
+    def pos(self: Self, value: pg.Vector2) -> None:
+        self._pos = value
 
 
 class Level(object):
