761daecb9a727ecd6abc6321627419562c8ab624
Tomatofu <99457089+tomatofoo@users.noreply.github.com>
2026-07-29 22:00:46 -0700

    update the stuff

--- modules/physics.py
+++ modules/physics.py
@@ -372,9 +372,9 @@ class Gon(Object):
                 if not obj._fixed:
                     obj._pos += rel
                 if not vertex1._fixed:
-                    vertex1._pos -= rel
+                    vertex1._pos -= rel * (1 - t)
                 if not vertex2._fixed:
-                    vertex2._pos -= rel
+                    vertex2._pos -= rel * t
 
     def _constrain(self: Self, objects: set[Object]) -> None:
         for obj in objects:
